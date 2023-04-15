34a8f21d60fcdad5d946a29f3705c6a58639f794
Sladriix-dev <sladriix@gmail.com>
2023-04-15 11:55:33 +0200

    ADD Movie list and Movie card components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-
 import Billboard from "@/components/Billboard";
+import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession } from "next-auth/react";
 
@@ -22,11 +23,16 @@ export async function getServerSideProps(context: NextPageContext) {
 }
 
 export default function Home() {
+  const { data: movies = [] } = useMovieList();
+
 
   return (
     <>
       <Navbar />
       <Billboard />
+      <div className="pb-40">
+        <MovieList title="Les Tendances" data={movies} />
+      </div>
     </>
     )
 }
